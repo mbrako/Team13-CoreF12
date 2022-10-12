9547cb4cab2d941d83a8de36e8d4df00b70948ca
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-11 23:51:37 -0700

    Added a new page for Request tab

---
+++ src/Pages/Request.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class RequestModel : PageModel
+    {
+        private readonly ILogger<RequestModel> _logger;
+
+        public RequestModel(ILogger<RequestModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
