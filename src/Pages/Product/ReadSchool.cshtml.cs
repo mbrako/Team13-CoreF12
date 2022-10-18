91f63ccc5b6e50b1e82dfa0684f95d7f92cfdff5
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-17 23:00:10 -0700

    Added a new Razor page ReadSchool.cshtml to the Product folder under Pages. This represents the static data of a school

---
+++ src/Pages/Product/ReadSchool.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class ReadSchoolModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
