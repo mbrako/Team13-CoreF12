b1fbbd7f40fe7f9e082509962684a16715cd8b5e
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-31 13:00:31 -0700

    Updated the ProductModel.cs to have correct fields

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -8,29 +8,22 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
 
-        [JsonPropertyName("img")]
-        public string Image { get; set; }
-
-        public string Url { get; set; }
+        [StringLength(maximumLength: 100, MinimumLength = 1, ErrorMessage = "The School Name should have a length of more than {2} and less than {1}")]
+        public string SchoolName { get; set; }
         
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
-        public string Title { get; set; }
+        [StringLength (maximumLength: 150, MinimumLength = 5, ErrorMessage = "The School Address should have a length of more than {2} and less than {1}")]
+        public string SchoolAddress { get; set; }
 
-        public string Description { get; set; }
+        [StringLength(maximumLength: 100, MinimumLength = 12, ErrorMessage = "The email should have a length of more than {2} and less than {1}")]
+        public string SchoolEmail { get; set; }
 
-        public int[] Ratings { get; set; }
-
-        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
-
-        public string Quantity { get; set; }
-
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-        public int Price { get; set; }
+        [StringLength(maximumLength: 13, MinimumLength = 10, ErrorMessage = "The email should have a length of more than {2} and less than {1}")]
+        public string SchoolContactInfo { get; set; }
 
         // Store the Comments entered by the users on this product
-        public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
+        [JsonPropertyName("Items")]
+        public List<ItemsModel> ItemsList { get; set; } = new List<ItemsModel>();
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
