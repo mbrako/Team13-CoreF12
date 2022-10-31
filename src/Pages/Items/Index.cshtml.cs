a6cf842e0582ddfa8a23552de0cdf6d0f2c1e482
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-30 23:02:00 -0700

    Moved Index.cshtml.cs under Items Folder

---
+++ src/Pages/Items/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Items
+{
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="itemsService"></param>
+        public IndexModel(JsonFileItemsService itemsService)
+        {
+            ItemsService = itemsService;
+        }
+
+        // Data Service
+        public JsonFileItemsService ItemsService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ItemsModel> Items { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+        public void OnGet()
+        {
+            Items = ItemsService.GetAllData();
+        }
+    }
+}
