5d107e1e59860979e8253f6dfff7749d2bd7840e
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-31 13:01:59 -0700

    Updated Index.cshtml.cs to read from JSON and ProductService

---
+++ src/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    /// <summary>
+    /// Index Page will return all the schools data to show
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="itemsService"></param>
+        public IndexModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
