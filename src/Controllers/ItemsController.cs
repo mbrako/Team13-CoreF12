eb8bdf1616d7cdf44e9acdedb23b4364caa1ee22
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-10-30 23:00:53 -0700

    Added a new controller for items

---
+++ src/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : Controller
+    {
+        public ItemsController(JsonFileItemsService itemsService)
+        {
+            ItemsService = itemsService;
+        }
+
+        public JsonFileItemsService ItemsService { get; }
+
+        [HttpGet]
+        public IEnumerable<ItemsModel> Get()
+        {
+            return ItemsService.GetAllData();
+        }
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            ItemsService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
