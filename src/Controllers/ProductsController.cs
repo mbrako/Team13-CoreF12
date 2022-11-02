b2b9322faf759aeb0564cacc955137c499f97520
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-01 22:12:16 -0700

    Added comments to methods in ProductController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -9,15 +9,23 @@ namespace ContosoCrafts.WebSite.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    /// <summary>
+    /// Products Controller to call the service methods to execute the required request
+    /// </summary>
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
-
+        // Data Service
         public JsonFileProductService ProductService { get; }
 
+        // Collection of the Data
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
