7984e034c998387d360e24e4aaec8d9d00402fe9
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-05 13:14:46 -0700

    Updated the comments for methods  in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,24 +8,33 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    ///<summary>
-    ///Shiva Chaithanya
-    ///</summary>
+
     /// <summary>
     /// Myke Brako
     /// </summary>
 
     /// <summary>
-    /// pranindhar reddy
-    /// </summary>
-
-    /// <summary>
     /// Vaishnavi Kulkarni
+    /// </summary>
+    /// 
+    /// <summary>
+    /// Index Page will return all the Items to show
     /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
+        // Items data Service
+        public JsonFileItemsService ItemsService { get; }
+
+        // Collection of data of Items
+        public IEnumerable<ItemsModel> Items { get; private set; }
+
+        /// <summary>
+        /// Default Constructor for IndexModel with JsonFileItemsService injected.
+        /// </summary>
+        /// <param name="itemsService"></param>
+        /// <param name="logger"></param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileItemsService itemsService)
         {
@@ -33,9 +42,9 @@ namespace ContosoCrafts.WebSite.Pages
             ItemsService = itemsService;
         }
 
-        public JsonFileItemsService ItemsService { get; }
-        public IEnumerable<ItemsModel> Items { get; private set; }
-
+        /// <summary>
+        /// REST OnGet, return data for all Items
+        /// </summary>
         public void OnGet()
         {
             Items = ItemsService.GetAllData();
