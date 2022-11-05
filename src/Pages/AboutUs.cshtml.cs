5e504b29d2ca59434f4d4dc6b7bec37a05b7e5a6
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-05 13:14:36 -0700

    Updated the comments for methods  in AboutUS.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -3,17 +3,17 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// The About US Page responsible to load the data of mission and vision
+    /// </summary>
     public class AboutUsModel : PageModel
     {
-        private readonly ILogger<AboutUsModel> _logger;
-
-        public AboutUsModel(ILogger<AboutUsModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
         }
+
     }
+
 }
