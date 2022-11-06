d8c08e3f33b155c799d05d5b5872b11d1597f51d
mbrako <m.brako@hotmail.com>
2022-11-05 22:36:37 -0700

    Updated comments in itemsModel.cs file

--- src/Models/ItemsModel.cs
+++ src/Models/ItemsModel.cs
@@ -12,30 +12,37 @@ namespace ContosoCrafts.WebSite.Models
     {
         // The ID for the items
         public string Id { get; set; }
-        public string Maker { get; set; }
 
+        // This is the link to the image file to display in item preview
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
+        // This is the web location for the item
         public string Url { get; set; }
 
+        // The Items Title
         [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
+        // A short description of the content of the item
         public string Description { get; set; }
 
+        // Ratings for the item which is a trace from the Contosco Crafts Website
         public int[] Ratings { get; set; }
 
+        // An enum to describe the the item category (tag)
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
         public string Quantity { get; set; }
 
+        // Item price which is a trace from the Contosco Crafts Website
         [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
 
-        // Store the Comments entered by the users on this product
+        // Store the Comments entered by the users on this items
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
+        // To string method to display the item as a text string
         public override string ToString() => JsonSerializer.Serialize<ItemsModel>(this);
 
     }
