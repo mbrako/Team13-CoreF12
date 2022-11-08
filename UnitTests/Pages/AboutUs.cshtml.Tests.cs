a6ed3add0789f16f5d12b7fe151d2c63bc092d30
mbrako <m.brako@hotmail.com>
2022-11-08 09:05:03 -0800

    Created Test cases for AboutUs.cshtml.cs

---
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.AboutUs
+{
+    public class PrivacyTests
+    {
+        #region TestSetup
+        public static AboutUsModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutUsModel>>();
+
+            pageModel = new AboutUsModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
