4f70db49fe8fe65c76f7abcbe96c721810a79777
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-08 00:57:09 -0800

    created test cases for Index.cshtml.cs under products

---
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+namespace UnitTests.Pages.Product.Index
+{
+    public class IndexTests
+    {
+        #region TestSetup
+        public static PageContext pageContext;
+
+        public static IndexModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new IndexModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            // Change this value if you modify the data source
+            Assert.AreEqual(4, pageModel.Products.ToList().Count);
+        }
+        #endregion OnGet
+    }
+}
