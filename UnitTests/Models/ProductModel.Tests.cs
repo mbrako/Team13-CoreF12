e1f87a8844cfdc5a55e9516052a49d20c5a2b0c1
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-08 00:56:44 -0800

    created test cases for ItemsModel.cs

---
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+using Bunit;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+
+
+
+//using ContosoCrafts.WebSite.Components;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Models
+{
+    public class ProductModelTests
+    {
+        #region TestSetup
+
+
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+
+
+        #endregion TestSetup
+
+
+
+
+        [Test]
+        public void ProductModel_Valid_ToString_Should_Return_String()
+        {
+            // Arrange
+            var data = new ProductModel() { SchoolName = "Seattle Public School" };
+
+
+
+            // Act
+            var result = data.ToString();
+
+
+
+            // Assert
+            Assert.AreEqual(true, result.Contains("Seattle Public School"));
+        }
+    }
+}
