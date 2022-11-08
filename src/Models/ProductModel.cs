22388caeb882cd1fa3ba912e69dcb0f964c9faae
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-07 22:34:08 -0800

    Updated the ProductModel.cs to contain correct values.

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -14,38 +14,48 @@ namespace ContosoCrafts.WebSite.Models
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
 
         // The school name
+        [Required]
         [StringLength(maximumLength: 100, MinimumLength = 1, ErrorMessage = "The School Name should have a length of more than {2} and less than {1}")]
         public string SchoolName { get; set; }
-        
+
         //The school address
+        [Required]
         [StringLength (maximumLength: 150, MinimumLength = 5, ErrorMessage = "The School Address should have a length of more than {2} and less than {1}")]
         public string SchoolAddress { get; set; }
 
         // The school's email
+        [Required]
         [StringLength(maximumLength: 100, MinimumLength = 12, ErrorMessage = "The  School Email should have a length of more than {2} and less than {1}")]
         public string SchoolEmail { get; set; }
 
         // The school's phone number (school contact info)
+        [Required]
         [StringLength(maximumLength: 13, MinimumLength = 10, ErrorMessage = "The School Phone Number should have a length of more than {2} and less than {1}")]
         public string SchoolContactInfo { get; set; }
 
         // The quantity of laptos needed
-        public string LaptopQuantity { get; set; }
+        [Range(0, 1000)]
+        public int LaptopQuantity { get; set; }
 
         // The quantity of projectors needed
-        public string ProjectorsQuantity { get; set; }
+        [Range(0, 1000)]
+        public int ProjectorsQuantity { get; set; }
 
         // The quantity of speakers needed
-        public string SpeakersQuantity { get; set; }
+        [Range(0, 1000)]
+        public int SpeakersQuantity { get; set; }
 
         // The quantity of smart boards needed
-        public string SmartBoardsQuantity { get; set; }
+        [Range(0, 1000)]
+        public int SmartBoardsQuantity { get; set; }
 
         // The quantity of tablets needed
-        public string TabletsQuantity { get; set; }
+        [Range(0, 1000)]
+        public int TabletsQuantity { get; set; }
 
         // The quantity of digital markers needed
-        public string DigitalMarkersQuantity { get; set; }
+        [Range(0, 1000)]
+        public int DigitalMarkersQuantity { get; set; }
 
         // To string method to display the items as a text string
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
