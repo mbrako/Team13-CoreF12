412764f9a517b1ac044f2954853c0639126e7592
mbrako <m.brako@hotmail.com>
2022-11-08 21:28:40 -0800

    Added comments to Read.cshtml.Tests.cs file

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -5,11 +5,19 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Read
 {
+
+    /// <summary>
+    /// This class holds the tests for the Read.cshtml.Tests.cs.
+    /// </summary>
     public class ReadTest
     {
         #region TestSetup
         public static ReadModel pageModel;
 
+
+        /// <summary>
+        /// Setup test prior to execution.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -21,6 +29,10 @@ namespace UnitTests.Pages.Product.Read
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Tests execution path when the school name does not exist
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
@@ -31,6 +43,9 @@ namespace UnitTests.Pages.Product.Read
             Assert.AreEqual("Mercer Island School", pageModel.Product.SchoolName);
         }
 
+        /// <summary>
+        /// Test valid result from OnGet method.
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products_ToString()
         {
