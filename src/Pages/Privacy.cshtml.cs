dca942573892d1665f914b22f241b4396a59eaa9
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-15 23:21:01 -0800

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,25 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// The Privacy page to display the privacy of the website
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        //The Ilogger member for logging data
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// The constructor with Ilogger dependency injection
+        /// </summary>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method to add actions to be performed on load of page
+        /// </summary>
         public void OnGet()
         {
         }
