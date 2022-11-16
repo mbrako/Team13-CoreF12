fdfe5bb9919e8ae573b3e1cb16c964bef3fb9a74
mbrako <m.brako@hotmail.com>
2022-11-15 22:44:37 -0800

    Added Privacy.cshtml.Tests.cs to Pages under Unit Tests

---
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Privacy
+{
+    /// <summary>
+    /// This class holds the tests for the Privacy.cshtml.Tests.cs.
+    /// </summary>
+    public class PrivacyTests
+    {
+        #region TestSetup
+        public static PrivacyModel pageModel;
+
+        /// <summary>
+        /// Setup test prior to execution.
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
+
+            pageModel = new PrivacyModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+
+        /// <summary>
+        /// Test a valid result from the OnGet method
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
