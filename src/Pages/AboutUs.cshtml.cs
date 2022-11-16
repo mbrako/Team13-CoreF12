9152033ea4e00b70bb2fad5f0ba6c64a77b4a184
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-15 23:20:39 -0800

    Added comments to AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -9,13 +9,20 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class AboutUsModel : PageModel
     {
+        //The Ilogger member for logging data
         private readonly ILogger<AboutUsModel> _logger;
 
+        /// <summary>
+        /// The constructor with Ilogger dependency injection
+        /// </summary>
         public AboutUsModel(ILogger<AboutUsModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method to add actions to be performed on load of page
+        /// </summary>
         public void OnGet()
         {
         }
