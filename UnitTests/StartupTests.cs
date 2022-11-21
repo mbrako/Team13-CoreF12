d1e96de2afd85ae0775e2d47bb2dd3327d9c6cd7
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-20 19:40:07 -0800

    Added comments to StartupTest.cs file

--- UnitTests/StartupTests.cs
+++ UnitTests/StartupTests.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 
 namespace UnitTests.Pages.Startup
 {
+    /// <summary>
+    /// This class invoke the Startup.cs to get the required service configurations.
+    /// </summary>
     public class StartupTests
     {
         #region TestSetup
@@ -14,12 +17,18 @@ namespace UnitTests.Pages.Startup
         {
         }
 
+        /// <summary>
+        /// Invoking the Startup.cs
+        /// </summary>
         public class Startup : ContosoCrafts.WebSite.Startup
         {
             public Startup(IConfiguration config) : base(config) { }
         }
         #endregion TestSetup
 
+        /// <summary>
+        /// Testing the configured services
+        /// </summary>
         #region ConfigureServices
         [Test]
         public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
@@ -29,6 +38,9 @@ namespace UnitTests.Pages.Startup
         }
         #endregion ConfigureServices
 
+        /// <summary>
+        /// Testing the configuration
+        /// </summary>
         #region Configure
         [Test]
         public void Startup_Configure_Valid_Defaut_Should_Pass()
