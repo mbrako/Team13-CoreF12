bd13047419f9979e5ce94d3bdf0156e5ef9e22dc
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-20 19:39:48 -0800

    Added comments to Program.cs file

--- src/Program.cs
+++ src/Program.cs
@@ -3,13 +3,19 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// This class contains the application startup code.
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        /// <summary>
+        /// This method creates a host builder object
+        /// </summary>
+        /// <returns>IHostBuilder object</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
@@ -17,4 +23,5 @@ namespace ContosoCrafts.WebSite
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
 }
