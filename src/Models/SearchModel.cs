f1ad0ac31a507543bf4f61f2e238f3cee816c0c5
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-29 02:15:41 -0800

    Renamed Search file to SearchModel

---
+++ src/Models/SearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class SearchModel
+    {
+        //A field to take user input for search string
+        public string Name { get; set; }
+
+        // To string method to display the items as a text string
+        public override string ToString() => JsonSerializer.Serialize<SearchModel>(this);
+    }
+}
