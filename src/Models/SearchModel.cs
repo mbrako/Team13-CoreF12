bd3e393eefc0cc6d7ab2d19f4a0426d041c25d81
Vaishnavi Kulkarni <vkulkarni@seattleu.edu>
2022-11-29 11:15:48 -0800

    Added annotataions for field validations in SearchModel.cs

--- src/Models/SearchModel.cs
+++ src/Models/SearchModel.cs
@@ -6,6 +6,8 @@ namespace ContosoCrafts.WebSite.Models
     public class SearchModel
     {
         //A field to take user input for search string
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 1, ErrorMessage = "The School Address should have a length of more than {2} and less than {1}")]
         public string Name { get; set; }
 
         // To string method to display the items as a text string
